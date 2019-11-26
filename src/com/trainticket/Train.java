1a5d0ef0f73b159dd84b2347ea66bf7bf61ce9d8
cldaly <clayton.daly91@gmail.com>
2019-11-26 15:31:13 -0500

    add classes

---
+++ src/com/trainticket/Train.java
@@ -0,0 +1,5 @@
+package com.trainticket;
+
+public class Train {
+
+}
---
+++ src/com/trainticket/TrainApplication.java
@@ -0,0 +1,10 @@
+package com.trainticket;
+
+public class TrainApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
