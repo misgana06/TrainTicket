20a5f4396fb2ab766fdd84f6c41fa4b1a81d32e1
Mohamed <mmomin12@student.gsu.edu>
2019-12-02 15:41:55 -0500

    index and gitignore

--- src/com/trainticket/Ticket.java
+++ src/com/trainticket/Ticket.java
@@ -1,5 +1,86 @@
 package com.trainticket;
 
-public class Ticket {
+import java.util.Date;
+import java.util.TreeMap;
 
+public class Ticket {
+	
+	private static int counter;
+	
+	private String pnr;
+	private Date travelDate;
+	private Train train;
+	private TreeMap<Passenger,Integer> passengers;
+	
+	
+	
+	public Ticket(Date travelDate, Train train) {
+		this.travelDate = travelDate;
+		this.train = train;
+	}
+	
+	public static int getCounter() {
+		return counter;
+	}
+	
+	public static void setCounter(int counter) {
+		Ticket.counter = counter;
+	}
+	
+	public String getPnr() {
+		return pnr;
+	}
+	
+	public void setPnr(String pnr) {
+		this.pnr = pnr;
+	}
+	
+	public Date getTravelDate() {
+		return travelDate;
+	}
+	
+	public void setTravelDate(Date travelDate) {
+		this.travelDate = travelDate;
+	}
+	
+	public Train getTrain() {
+		return train;
+	}
+	
+	public void setTrain(Train train) {
+		this.train = train;
+	}
+	
+	public TreeMap<Passenger, Integer> getPassengers() {
+		return passengers;
+	}
+	
+	public void setPassengers(TreeMap<Passenger, Integer> passengers) {
+		this.passengers = passengers;
+	}
+	
+	
+	public String generatePNR() {
+		return "";
+	}
+	
+	public double calcPassengerFare(Passenger p) {
+		return 0.00;
+	}
+	
+	public void addPassenger(String name, int age, char gender) {
+		
+	}
+	
+	public double calculateTotalTicketPrice() {
+		return 0.00;
+	}
+	
+	public StringBuilder generateTicket() {
+		return null;
+	}
+	
+	public void writeTicket() {
+		
+	}
 }
